6f3fd805d06e2d8a7fe093cea8fe588ac7ed1221
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-15 01:37:06 +0300

    asd

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -22,7 +22,7 @@ namespace DegerVeReferansTipler
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
             Console.WriteLine(sayilar1[0]); //999
-            // pointer vs cop toplayıcı kavramları 
+            // pointer vs cop toplayıcı kavramlarıı
 
 
 
---
+++ OOP1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.ProductName = "masa";
+            product1.CategoryId = 2;
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Update(product1);
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
