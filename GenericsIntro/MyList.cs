bde6dec085240aa20a6b27895e2356c41b490cfd
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-19 04:40:09 +0300

    update

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
 
 namespace GenericsIntro
 {
     internal class MyList<T>
     {
+        T[] items;
+        // constructorS
+        public MyList()
+        {
+            items= new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items=new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i]=tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+        public int Lenght
+        { 
+            get { return items.Length; }
+
+        }
     }
 }
--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Documents;
 
-namespace GenericsIntro
+namespace OOP2
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-
-            MyList<string> isimler = new MyList<string>();
-            isimler.Add("rukiye");
 
             
 
